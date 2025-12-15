dd6c221620174da8a20ba1b4ba49cf61f00677ac
User <user@example.com>
2025-12-15 21:54:38 +0100

    Defer CSS and Lottie script to improve LCP and reduce critical path latency

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'defer-css',
+      transformIndexHtml(html) {
+        return html.replace(
+          /<link rel="stylesheet" crossorigin href="(\/assets\/.*\.css)">/,
+          '<link rel="preload" href="$1" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">'
+        );
+      }
+    }
+  ],
 })
